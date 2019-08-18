436221331d64a4b60c3aa9fa6ccec3cd82c0146a
Ameenah_Oduola <aoduola@appzonegroup.com>
2019-08-18 15:30:27 +0100

    Task 2: Capitalize/uncapitalize filenames in a directory, more comments added

--- check_file.py
+++ check_file.py
@@ -2,10 +2,11 @@
 # -S to captalize file names in a directory
 # or -s to change the file names to lowercase
 
-# importing os module
+# imports
 import os
 import argparse
 
+#Parser to fetch command line arguements
 parser = argparse.ArgumentParser()
 group = parser.add_mutually_exclusive_group(required=True)
 group.add_argument("-s", help="change all the filename of the files in a directory to "
@@ -14,38 +15,35 @@ group.add_argument("-S", help="change all the filename of the files in a directo
                               "capital letter")
 args = parser.parse_args()
 
-# Function to rename multiple files
+# Function to change all the filename of the files in a directory to
+# capital letter
 def capitalize_filenames(directory):
     for filename in os.listdir(directory):
         dst =filename.upper()
         src =directory+ filename
         dst =directory+ dst
 
-        # rename() function will
-        # rename all the files
+        # rename the file
         os.rename(src, dst)
 
-# Function to rename multiple files
+# Function to change all the filename of the files in a directory to
+# small letter
 def uncapitalize_filenames(directory):
     for filename in os.listdir(directory):
         dst =filename.lower()
         src =directory+ filename
         dst =directory+ dst
 
-        # rename() function will
-        # rename all the files
+        # rename the file
         os.rename(src, dst)
 
-if args.s !="None":
+#checks command line argument to capitalize or uncapitalize file names
+if args.s !=None:
     uncapitalize_filenames(args.s)
-elif args.S !="None":
+    print("Successful")
+elif args.S !=None:
     capitalize_filenames(args.S)
+    print("Successful")
 else:
     print("Please enter a valid argument")
 
-
-# Driver Code
-#if __name__ == '__main__':
-
-    # Calling main() function
-#    capitalize_filenames()
---
+++ test_redis.py
@@ -0,0 +1,9 @@
+import redis
+myHostname = "<Your Host Name>.redis.cache.windows.net"
+myPassword = "<Your Access Key>"
+
+r = redis.StrictRedis(host=myHostname, port=6380,
+                      password=myPassword, ssl=True)
+
+result = r.ping()
+print("Ping returned : " + str(result))
